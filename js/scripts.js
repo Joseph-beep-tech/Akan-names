a8409a374c9d0a7871a3c2075cbda687102ba077
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-10-02 23:46:08 +0300

    pushed changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 function name() {
-var 
+var year = document.getElementById("nam").Value;
+var month = document.getElementById("mon").Value;
+var day = document.getElementById("dat").Value;
+var date = new Date(`${year}-${month}-${day}`);
+
+
+
+
+
+var daysWeek = ["Sunday", "Monday", "Tuesday," "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Awoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",];
 }
+if (year == "" || year.length < 4 || year.length > 4) {} 
