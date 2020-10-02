37ff3b9bf2124f0887469605b9bad24399347505
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-10-03 01:33:57 +0300

    js file modifications

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,26 @@ var year = document.getElementById("nam").Value;
 var month = document.getElementById("mon").Value;
 var day = document.getElementById("dat").Value;
 var date = new Date(`${year}-${month}-${day}`);
-
-
-
-
-
-var daysWeek = ["Sunday", "Monday", "Tuesday," "Wednesday", "Thursday", "Friday", "Saturday"];
+var ground = date.getDay();
+var male = document.getElementById("male");
+var female = document.getElementById("female");
+var bring = document.getElementById("possible");
+var daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Awoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",];
 }
-if (year == "" || year.length < 4 || year.length > 4) {} 
+if (year == "" || year.length < 4 || year.length > 4) {
+    alert(" Please enter a valid year!");
+} else if (month < 0 || month > 12) {
+    alert("Please enter a valid month!");
+} else if (day < 0 || day > 31) {
+    alert("Please enter a valid date!");
+} else if (male.checked == false && female.checked == false) {
+    alert("Please specify your gender!");
+} else if (male.checked == true) {
+    bring.innerHTML = "Congrats" + maleNames[ground] + ". your birth day is on "  + daysWeek[ground] + ". and that's your Akan Name.";
+} else if (female.checked == true) {
+    bring.innerHTML = "Congrats" + femaleNames[ground] + ". your birth day is on " + daysWeek[ground] + ". and that's your Akan Name."
+}   else {
+    alert("Contnue");
+}
