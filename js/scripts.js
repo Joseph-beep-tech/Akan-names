dc658a6060e193e26fd26aa3b59c3b171e0df79b
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-10-04 01:46:21 +0300

    modification on java script

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
-function name() {
-var year = document.getElementById("nam").Value;
-var month = document.getElementById("mon").Value;
-var day = document.getElementById("dat").Value;
-var date = new Date(`${year}-${month}-${day}`);
- var  = date.getDay();
- var male = document.getElementById("male");
- var female = document.getElementById("female");
- var result = document.getElementById("result");
- var daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
- var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
- var femaleNames = ["Akosua", "Awoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",];
- }
- if (year == "" || year.length < 4 || year.length > 4) {
-     alert(" Please enter a valid year!");
- } else if (month < 0 || month > 12) {
-     alert("Please enter a valid month!");
- } else if (day < 0 || day > 31) {
-     alert("Please enter a valid date!");
- } else if (male.checked == false && female.checked == false) {
-     alert("Please specify your gender!");
- } else if (male.checked == true) {
-     result.innerHTML = "Congrats" + maleNames[ground] + ". your birth day is on "  + daysWeek[ground] + ". and that's your Akan Name.";
- } else if (female.checked == true) {
-     result.innerHTML = "Congrats" + femaleNames[ground] + ". your birth day is on " + daysWeek[ground] + ". and that's your Akan Name.";
- }   else {
-     alert("Contnue");
+function findZinc() {
+    var year = document.getElementById("nam").value;
+    var month = document.getElementById("mon").value;
+    var day = document.getElementById("dat").value;
+    var date = new Date(`${year}-${month}-${day}`);
+    var myDay = date.getDay();
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    var result = document.getElementById("result");
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Kwabena", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "AKua", "Yaa", "Afua", "Ama"];
+    if (year == "" || year.length < 4 || year.length > 4) {
+        alert("Please enter a valid year!");
+    } else if (month <= 0 || month > 12) {
+        alert("Please enter a valid month!");
+    } else if (day <= 0 || day > 31) {
+        alert("Please enter a valid date!");
+    } else if (male.checked == false && female.checked == false) {
+        alert("Please specify your gender!");
+    } else if (male.checked == true) {
+        result.innerHTML = "Congrats " + maleNames[myDay] + ". you were born on a " + days[myDay] + " and that's your Akan Name.";
+    } else if (female.checked == true) {
+        result.innerHTML = "Hey " + femaleNames[myDay] + ". you were born on a " + days[myDay] + " and that's your Akan Name.";
+        alert("Please press ok to find out your Akan Name!")
+    }
 }
